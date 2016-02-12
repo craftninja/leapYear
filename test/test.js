5ecdae07817fd0e90906a9666c9e7b150d480ccb
Emily Platzer <emilyplatzer@gmail.com>
2016-02-12 16:12:40 -0700

    leapYear works for lotsa years, mmmkay

--- test/test.js
+++ test/test.js
@@ -2,12 +2,17 @@ var code = require('../main.js');
 var expect = require('chai').expect;
 
 describe("leapYear", function() {
+
   it("returns true for 2016", function() {
     expect(code.leapYear(2016)).to.equal(true);
   });
   it("returns false for 2015", function () {
     expect(code.leapYear(2015)).to.equal(false);
   })
-  it("returns true for 1600")
-  it("returns false for 1700")
+  it("returns true for 1600", function () {
+    expect(code.leapYear(1600)).to.equal(true);
+  })
+  it("returns false for 1700", function () {
+    expect(code.leapYear(1700)).to.equal(false);
+  })
 });
