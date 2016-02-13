6cf9178beeadd16e4c048e3c0c9b48896245b44a
Emily Platzer <emilyplatzer@gmail.com>
2016-02-12 18:17:22 -0700

    leapYear can calculate leapness...
    Years divisible by 4 are leap, unless they are divisible by 100 (not leap), but are still leap if divisible by 400. Whew

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 module.exports = {
   leapYear: function (year) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
   }
 };
